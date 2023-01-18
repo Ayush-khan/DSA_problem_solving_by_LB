44f0b00ae799e473bb63ee63fcfc7ed91feb325b
FIRST_NAME LAST_NAME <ayushkhana230@gmail.com>
2023-01-18 23:37:03 +0530

    modified and sloved an error in the same file

--- max_consecutive_ones.cpp
+++ max_consecutive_ones.cpp
@@ -1,42 +1,50 @@
 // Given a binary array nums, return the maximum number of consecutive 1's in the array.?
-#include<iostream>
-#include<vector>
+#include <iostream>
+#include <vector>
 using namespace std;
-class Solution {
+class Solution
+{
 public:
-    int findMaxConsecutiveOnes(vector<int>& nums) {
-        int count=0;
-        int count1=0;
-        for(int i=0;i<nums.size();i++){
-            if(nums[i]==1){
+    int findMaxConsecutiveOnes(vector<int> &nums)
+    {
+        int count = 0;
+        int count1 = 0;
+        for (int i = 0; i < nums.size(); i++)
+        {
+            if (nums[i] == 1)
+            {
                 // if(nums[i+1]==1)
                 count++;
             }
-            else{
-                if(count>count1 && count!=0){
-                count1=count;
-             
-                count=0;  
-                 }
+            else
+            {
+                // if(count>count1 && count!=0){
+                // count1=count;
+
+                count = 0;
+            }
+            if (count > count1)
+            {
+                count1 = count;
             }
         }
-        if(count>count1){
-            return count;
-        }
-        else{
-            return count1;
-        }
+        return count1;
     }
+    // if(count>count1){
+    //     return count;
+    // }
+    // else{
+    //     return count1;
+    // }
 };
 int main()
-{ 
+{
     // int target;
-    vector<int> v1={1,1,1,1,0,0,0,1,1,0,1,1,1,1,0,0,1,1};
+    vector<int> v1 = {1, 1, 1,1,1,1, 1, 0, 0, 0, 1, 1, 0, 1, 1, 1, 1, 0, 0, 1, 1};
     cout << "Enter the targeted element of an array" << endl;
     // cin >> target;
     Solution s1;
-    int majority_Element=s1.findMaxConsecutiveOnes(v1);
+    int majority_Element = s1.findMaxConsecutiveOnes(v1);
     // cout<<endl<<"The majority Element in an array is:"<<endl;
-     cout<<"majority_Element : "<<majority_Element<<endl;
-    
+    cout << "majority_Element : " << majority_Element << endl;
 }
