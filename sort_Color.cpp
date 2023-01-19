429798d23f70fe63c967097bcf892f88d74a3762
FIRST_NAME LAST_NAME <ayushkhana230@gmail.com>
2023-01-19 23:52:05 +0530

    modified in the sort colors file"

---
+++ sort_Color.cpp
@@ -0,0 +1,20 @@
+// Given an array nums with n objects colored red, white, or blue, sort them in-place so that objects of the same color are adjacent, with the colors in the order red, white, and blue.
+
+// We will use the integers 0, 1, and 2 to represent the color red, white, and blue, respectively.
+// 
+// You must solve this problem without using the library's sort function.
+#include<iostream>
+#include<vector>
+using namespace std;
+class Solution {
+public:
+    void sortColors(vector<int>& nums) {
+        
+    }
+};
+int main(){
+    vector<int> v1={2,0,2,1,1,0};
+    Solution s1;
+    s1.sortColors(v1);
+
+}
