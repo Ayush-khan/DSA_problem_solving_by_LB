c0f7712b6854c6b7320a6fbf351a889b509dd4d6
FIRST_NAME LAST_NAME <ayushkhana230@gmail.com>
2023-01-22 00:16:19 +0530

    modified

--- moves_zeros.cpp
+++ moves_zeros.cpp
@@ -1,25 +1,26 @@
 #include <iostream>
 #include <vector>
-#include<algorithm>
+#include <algorithm>
 using namespace std;
 class Solution
 {
 public:
     void moveZeroes(vector<int> &nums)
-    {   sort(nums.begin(),nums.end());
+    {
+        sort(nums.begin(), nums.end());
         vector<int>::iterator it = nums.begin();
         cout << "HELLOO" << endl;
         int i = 0;
-        for(;i<nums.size();i++){
-            if(nums[i]!=0){
+        for (; i < nums.size(); i++)
+        {
+            if (nums[i] != 0)
+            {
                 break;
             }
-            // else{
-            //     nums.erase(nums.begin()+i);
-            // }
         }
-        nums.erase(nums.begin(),nums.begin()+i);
-        for(int j=0;j<i;j++){
+        nums.erase(nums.begin(), nums.begin() + i); // remove the stating zeros
+        for (int j = 0; j < i; j++)
+        {
             nums.push_back(0);
         }
         // logic doesn't work
@@ -28,33 +29,46 @@ public:
         //     if (nums[i] == 0)
         //     {  nums.push_back(0);
         //         nums.erase(nums.begin() + i);
-              
-                
+
         //     }
         //     else{
         //         i++;
         //     }
         // }
 
-        // while(it!=nums.end()){
-        //     cout<<"INsise"<<endl;
-        //     if(*it==0){
-        //         nums.erase(nums.begin()+it);
+        // while (it != nums.end())
+        // {
+        //     cout << "INsise" << endl;
+        //     if (*it == 0)
+        //     {
+        //     // {cout<<"*it = "<<*it<<" ";
+        //         nums.erase(nums.begin() + i);
         //         nums.push_back(0);
+        //         if(nums[i+1]==0){
+        //             i=i-1;
+        //             it=it-1;
+        //         }
+        //         // it=it-1;
+        //         // i=i-1;
         //     }
-        //     it++;
+        //     // else if(*it!=0){
+        //     //      i++;
+        //     // it++; 
+        //     // }
+        //         i++;
+        //     it++; 
+            
         // }
-        while (it != nums.end())
+        for(int i=0;i<nums.size();i++)
         {
-            cout << *it << " ";
-            it++;
+            cout << nums[i] << " ";
+            
         }
     }
 };
 int main()
 {
-    vector<int> v1 = {0, 1, 0,0,0,0,3, 12};
-    // int arr[] = {2, 0, 2, 1, 1, 0};
+    vector<int> v1 = {0, 1, 0, 0, 0, 0, 3, 12};
     Solution s1;
     int siz;
     cout << " Starting..." << endl;
--- tempCodeRunnerFile.cpp
+++ tempCodeRunnerFile.cpp
@@ -1,1 +1,12 @@
- // if(nums[i+1]==1)
+for (; i < nums.size(); i++)
+        {
+            if (nums[i] != 0)
+            {
+                break;
+            }
+        }
+        nums.erase(nums.begin(), nums.begin() + i); // remove the stating zeros
+        for (int j = 0; j < i; j++)
+        {
+            nums.push_back(0);
+        }
