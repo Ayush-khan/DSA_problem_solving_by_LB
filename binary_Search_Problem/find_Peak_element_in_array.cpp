62dbbc1b77c1e26db193324cdc044b704d4de85a
FIRST_NAME LAST_NAME <ayushkhana230@gmail.com>
2023-02-10 12:50:12 +0530

    solve using binary search

--- binary_Search_Problem/find_Peak_element_in_array.cpp
+++ binary_Search_Problem/find_Peak_element_in_array.cpp
@@ -6,15 +6,29 @@ using namespace std;
 class Solution{
     public:
         int peakIndexInMountainArray(vector<int>& arr) {
-        sort(arr.begin(),arr.end());
-        int max=arr.size()-1;
-        return arr[max];
+            // way to find maximum number
+        // sort(arr.begin(),arr.end());
+        // int max=arr.size()-1;
+        // return arr[max];
+        int s=0;
+        int e=arr.size()-1;
+        while(s<e){
+            int mid=(s+e)/2;
+            if(arr[s]<arr[mid]){
+                s=mid;
+                // e=mid;
+            }
+            else{
+                e=mid;
+            }
+        }
+        return s;
     }
 
 };
 int main(){
-    vector<int>v1={0,10,3,2,1};
+    vector<int>v1={0,1,4,6,7,9,10,23,24,25,26,27,22,11,1,0};
     Solution s1;
    int ans= s1.peakIndexInMountainArray(v1);
-   cout<<"THe answer is: "<<ans<<endl;
+   cout<<"THe index is:["<<ans<<"]= "<<v1[ans]<<endl;
 }
