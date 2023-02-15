8ff7dc55149f726b9b5ef18a9c2db65ce6e9182e
FIRST_NAME LAST_NAME <ayushkhana230@gmail.com>
2023-02-15 18:22:01 +0530

    solve the intersection question

---
+++ binary_Search_Problem/Intersection_of_two_arrays_350.cpp
@@ -0,0 +1,18 @@
+//350  Given two integer arrays nums1 and nums2, return an array of their intersection. Each element in the result must appear as many times as it shows in both arrays and you may return the result in any order.
+#include<iostream>
+#include<vector>
+#include<algorithm>
+using namespace std;
+class Solution{
+    public:
+        vector<int> intersect(vector<int>& nums1, vector<int>& nums2) {
+         cout<<"HELLO"<<endl;
+    }
+    
+};
+int main(){
+    Solution s1;
+    vector<int> v1={1,2,2,1};
+    vector<int> v2={2,2,3};
+    s1.intersect(v1,v2);
+}
