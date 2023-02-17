a9a903c96549bcbe19de38f3daa81f4eb43884b1
FIRST_NAME LAST_NAME <ayushkhana230@gmail.com>
2023-02-17 22:27:49 +0530

    modified

--- binary_Search_Problem/separate_the_Digits_in_array_2553.cpp
+++ binary_Search_Problem/separate_the_Digits_in_array_2553.cpp
@@ -12,46 +12,63 @@ class Solution
 public:
     vector<int> separateDigits(vector<int> &nums)
     {
-        vector<int> s1;
+        // method using string to solve this questions
+        vector<int> ans;
         for (int i = 0; i < nums.size(); i++)
         {
-            int Digit = nums[i];
-            if (Digit >= 9)
-            {    
-                while (Digit > 0)
-                {
-
-                    cout << "Inside while loop" << endl;
-                    int lastDigit=Digit;
-                    if(lastDigit>=9){
-                    lastDigit=lastDigit/10;
-                    int put=lastDigit%10;
-                    cout<<"put= "<<put<<endl;
-                    s1.push_back(put);
-                    }
-                    else{
-                        cout<<"ELse digit"<<lastDigit<<endl;
-                         s1.push_back(lastDigit);
-                    }
-                    Digit=Digit/10;
-                    // Digit = Digit / 10;
-                }
-            }
-            else
+            string num = to_string(nums[i]);
+            for (int j = 0; j < num.size(); j++)
             {
-                s1.push_back(Digit);
+                ans.push_back(num[j] - '0');
             }
         }
-        return s1;
+        return ans;
+        // i am trying to solve it but some error is thrown due to which answer is not matched
+        // vector<int> s1;
+        // for (int i = 0; i < nums.size(); i++)
+        // {
+        //     int Digit = nums[i];
+        //     if (Digit >= 9)
+        //     {
+        //         while (Digit > 0)
+        //         {
+
+        //             cout << "Inside while loop" << endl;
+        //             int lastDigit=Digit;
+        //             if(lastDigit>=9){
+        //             lastDigit=lastDigit/10;
+        //             int put=lastDigit%10;
+        //             cout<<"put= "<<put<<endl;
+        //             s1.push_back(put);
+        //             }
+        //             else{
+        //                 cout<<"ELse digit"<<lastDigit<<endl;
+        //                  s1.push_back(lastDigit);
+        //             }
+        //             Digit=Digit/10;
+        //             // Digit = Digit / 10;
+        //         }
+        //     }
+        //     else
+        //     {
+        //         s1.push_back(Digit);
+        //     }
+        // }
+        // return s1;
     }
 };
 int main()
 {
     Solution s1;
-    vector<int> v1 = {48763, 25, 83, 4,77};
+    vector<int> v1 = {48763, 25, 83, 4, 77};
     vector<int> result = s1.separateDigits(v1);
+    cout << "Vector:= ";
     for (int i = 0; i < result.size(); i++)
     {
-        cout << result[i] << " ";
+        cout << "[" << result[i] << "]";
+        if (i < result.size() - 1)
+        {
+            cout << ",";
+        }
     }
 }
