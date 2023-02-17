74947892129b66c5599eb806fed2cc70fa5cb504
FIRST_NAME LAST_NAME <ayushkhana230@gmail.com>
2023-02-17 09:43:55 +0530

    error can't be resolved now after trying to much

--- binary_Search_Problem/Intersection_of_two_arrays_350.cpp
+++ binary_Search_Problem/Intersection_of_two_arrays_350.cpp
@@ -5,32 +5,46 @@
 using namespace std;
 class Solution
 {
+    // Some error detected now at this time 
 public:
     vector<int> intersect(vector<int> &nums1, vector<int> &nums2)
-    {
+    {    
         sort(nums1.begin(), nums1.end());
-        for(int i=0;i<nums1.size();i++){
-            cout<<nums1[i]<<" ";
+        for (int i = 0; i < nums1.size(); i++)
+        {
+            cout << nums1[i] << " ";
         }
-        cout<<endl;
+        cout << endl;
         sort(nums2.begin(), nums2.end());
-        for(int i=0;i<nums2.size();i++){
-            cout<<nums2[i]<<" ";
+        for (int i = 0; i < nums2.size(); i++)
+        {
+            cout << nums2[i] << " ";
         }
-        cout<<endl;
+        cout << endl;
         vector<int> v1;
-        for (int a = 0, b = 0; a < nums1.size() && b < nums2.size(); a++)
+        int minSize = (nums1.size()) >= (nums2.size()) ? (nums2.size()) : (nums1.size());
+        cout << "THe size of larger array is: " << minSize<< endl;
+        for (int a = 0, b = 0; a < nums1.size() && b < nums2.size();)
         {
             if (nums1[a] == nums2[b])
             {
                 v1.push_back(nums1[a]);
-                if (nums1[a] == nums1[a + 1])
-                {
-                    a++;
-                }
+                // if (nums1[a] == nums1[a + 1])
+                // {
+                //     a++;
+                //     if(nums2[b]==nums2[b+1]){
+                //         b++;
+                //     }
+                // }
+                a++;
                 b++;
             }
-            b++;
+            else{
+                
+                a++;
+                // b++;
+            }
+            
             // for (int b = 0; b < nums2.size();b++ )
             // {
             //     if (nums1[a] == nums2[b])
@@ -51,8 +65,8 @@ int main()
 {
     cout << "WE are started now " << endl;
     Solution s1;
-    vector<int> v1 = {1, 2, 3,8,6,5,4,3,2, 4, 5, 5, 5, 5, 2, 1};
-    vector<int> v2 = {2, 4, 4, 4, 5, 8,4,2,1,5, 5, 1, 3, 2, 3};
+    vector<int> v1 = {9,65,4,3,2,7,7,7,5,54,1,2,3,4,5,6,7,8,9};
+    vector<int> v2 = {8,1,3,2,4,5,6,7,8,65,54,2};
     vector<int> result = s1.intersect(v1, v2);
     for (int i = 0; i < result.size(); i++)
     {
