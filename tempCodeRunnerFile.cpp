0bc5769ec9b3633c71467a7a775de9a31c21576a
FIRST_NAME LAST_NAME <ayushkhana230@gmail.com>
2023-03-27 22:16:08 +0530

    modified the code and correct the error

--- tempCodeRunnerFile.cpp
+++ tempCodeRunnerFile.cpp
@@ -1,12 +1,18 @@
-for (; i < nums.size(); i++)
-        {
-            if (nums[i] != 0)
-            {
-                break;
-            }
-        }
-        nums.erase(nums.begin(), nums.begin() + i); // remove the stating zeros
-        for (int j = 0; j < i; j++)
-        {
-            nums.push_back(0);
-        }
+#include<iostream>
+#include<vector>
+using namespace std;
+int main(){
+    vector<int> nums;
+for (int i=0; i < nums.size(); i++)
+{
+    if (nums[i] != 0)
+    {
+        break;
+    }
+}
+nums.erase(nums.begin(), nums.begin() + i); // remove the stating zeros
+for (int j = 0; j < i; j++)
+{
+    nums.push_back(0);
+}
+}
