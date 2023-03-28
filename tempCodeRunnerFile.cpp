3525ca1b917b97619484e795ea144564b3536877
FIRST_NAME LAST_NAME <ayushkhana230@gmail.com>
2023-03-28 23:23:54 +0530

    modified

--- tempCodeRunnerFile.cpp
+++ tempCodeRunnerFile.cpp
@@ -1,19 +1,21 @@
-#include<iostream>
-#include<vector>
+// Main program
+#include <iostream>
+#include <vector>
 using namespace std;
-int main(){
+int main()
+{
     vector<int> nums;
-    int i=0;
-for (; i < nums.size(); i++)
-{
-    if (nums[i] != 0)
+    int i = 0;
+    for (; i < nums.size(); i++)
     {
-        break;
+        if (nums[i] != 0)
+        {
+            break;
+        }
     }
-}
-nums.erase(nums.begin(), nums.begin() + i); // remove the stating zeros
-for (int j = 0; j < i; j++)
-{
-    nums.push_back(0);
-}
+    nums.erase(nums.begin(), nums.begin() + i); // remove the stating zeros
+    for (int j = 0; j < i; j++)
+    {
+        nums.push_back(0);
+    }
 }
