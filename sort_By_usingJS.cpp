af9f88da1908e368f6a48b754defb69415e94938
FIRST_NAME LAST_NAME <ayushkhana230@gmail.com>
2023-04-08 13:23:21 +0530

    modified

--- sort_By_usingJS.cpp
+++ sort_By_usingJS.cpp
@@ -4,19 +4,21 @@ using namespace std;
 int main(){
     int arr[]={23,45,11,1,2,3,432};
     for(int i=0;i<sizeof(arr)/sizeof(arr[0]);){
-        if(arr[i]-arr[i+1]<=0){
+        if(arr[i]-arr[i+1]<=0)
+        {
             i++;
-            
         }
-        else{
+        else
+        {
+            // else statement
             int temp=arr[i];
             arr[i]=arr[i+1];
             arr[i+1]=temp;
             i++;
+            
         }
     }
      for(int i=0;i<sizeof(arr)/sizeof(arr[0]);i++){
-     cout<<arr[i]<<" ";
+        cout<<arr[i]<<" ";
      }
-
 }
