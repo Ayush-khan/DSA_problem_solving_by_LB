fecc6d26f8007917636cb0b08f8f247e2201ab36
FIRST_NAME LAST_NAME <ayushkhana230@gmail.com>
2023-04-16 23:00:52 +0530

    added new file and done first commit on it

---
+++ new_development.cpp
@@ -0,0 +1,6 @@
+// This is the new development file for increase the github commits
+#include<iostream>
+using namespace std;
+int main(){
+    cout<<"This file is used to increase the commi in the only in ramdan git hub";
+}
